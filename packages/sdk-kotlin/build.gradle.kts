525030390fdce6764e3803b371e7c11035ffe83b
V. Furkan <vfurkanguner@gmail.com>
2026-05-27 11:56:08 +0300

    feat(sdk-kotlin): add kotlinx-coroutines-core + kotlinx-coroutines-test

--- packages/sdk-kotlin/build.gradle.kts
+++ packages/sdk-kotlin/build.gradle.kts
@@ -11,8 +11,10 @@ repositories {
 
 dependencies {
     implementation("net.java.dev.jna:jna:5.14.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
